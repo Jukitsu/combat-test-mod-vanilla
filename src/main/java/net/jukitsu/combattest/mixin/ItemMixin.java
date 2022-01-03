09e9d3ddac3fcc642a12cda8b5854e87fe79ec91
BuildTools <unconfigured@null.spigotmc.org>
2022-01-02 21:57:55 -0500

    Created item cooldowns for eggs and snowballs.
    Added cooldowns for eggs and snowballs.  This could not be done in the item class, so two classes were created for each item to have their respective cooldowns.

--- src/main/java/net/jukitsu/combattest/mixin/ItemMixin.java
+++ src/main/java/net/jukitsu/combattest/mixin/ItemMixin.java
@@ -1,15 +1,20 @@
 package net.jukitsu.combattest.mixin;
 
-import net.minecraft.world.item.BowlFoodItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Item.class)
-public class BowlFoodItemMixin {
+public abstract class ItemMixin {
+    @Shadow public abstract Item asItem();
+
     @Inject(method = "getUseDuration", at = @At(value = "HEAD"), cancellable = true)
     public void changeUseDuration(ItemStack itemStack, CallbackInfoReturnable<Integer> cir)
     {
---
+++ src/main/java/net/jukitsu/combattest/mixin/SnowballItemMixin.java
@@ -0,0 +1,26 @@
+package net.jukitsu.combattest.mixin;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.EggItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SnowballItem;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SnowballItem.class)
+public class SnowballItemMixin {
+    @Inject(method = "use", at = @At(value = "HEAD"))
+    public void addProjectileCooldown(Level level, Player player, InteractionHand interactionHand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir)
+    {
+
+        player.getCooldowns().addCooldown( (Item) (Object)  this, 4);
+
+    }
+
+}
