d8927f6785e58f9d4e9032ed86341521b65de8b9
BuildTools <unconfigured@null.spigotmc.org>
2022-01-03 12:08:07 -0500

    Update LivingEntityMixin.java
    Fixed a bug with shields that made axes do twice as much damage instead of blocking it.

--- src/main/java/net/jukitsu/combattest/mixin/LivingEntityMixin.java
+++ src/main/java/net/jukitsu/combattest/mixin/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package net.jukitsu.combattest.mixin;
 
-import net.minecraft.world.InteractionHand;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.damagesource.CombatTracker;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -10,21 +11,29 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ShieldItem;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+
+    @Shadow
+    protected ItemStack useItem;
+    @Shadow
+    @Nullable
+    private DamageSource lastDamageSource;
+
 
     protected LivingEntityMixin(EntityType<? extends LivingEntity> entityType, Level level) {
         super(entityType, level);
@@ -33,22 +42,40 @@ public abstract class LivingEntityMixin extends Entity {
     @Shadow
     public abstract double getAttributeValue(Attribute attr);
 
+    @Shadow
+    public abstract boolean isDamageSourceBlocked(DamageSource damageSource);
 
-    @Shadow public abstract boolean isDamageSourceBlocked(DamageSource damageSource);
+
+    @Shadow
+    public abstract boolean isUsingItem();
+
+
+    @Shadow
+    protected abstract void blockUsingShield(LivingEntity livingEntity);
+
+    @Shadow
+    protected abstract void actuallyHurt(DamageSource damageSource, float f);
+
+
+    @Shadow public abstract boolean isDeadOrDying();
+
+    @Shadow protected abstract boolean checkTotemDeathProtection(DamageSource damageSource);
+
+    @Shadow @Nullable protected abstract SoundEvent getDeathSound();
+
+    @Shadow protected abstract float getSoundVolume();
+
+    @Shadow public abstract float getVoicePitch();
+
+    @Shadow public abstract void die(DamageSource damageSource);
+
+    @Shadow protected abstract void playHurtSound(DamageSource damageSource);
 
     @Shadow @Nullable public abstract DamageSource getLastDamageSource();
 
-    @Shadow public abstract boolean isUsingItem();
+    @Shadow @Final private CombatTracker combatTracker;
 
-    @Shadow protected ItemStack useItem;
-
-    @Shadow public abstract ItemStack getItemInHand(InteractionHand interactionHand);
-
-    @Shadow protected abstract void blockUsingShield(LivingEntity livingEntity);
-
-    @Shadow protected abstract void actuallyHurt(DamageSource damageSource, float f);
-
-    @Shadow protected abstract float getDamageAfterMagicAbsorb(DamageSource damageSource, float f);
+    @Shadow public abstract float getHealth();
 
     @Overwrite
     public void knockback(double d, double e, double f) {
@@ -61,39 +88,53 @@ public abstract class LivingEntityMixin extends Entity {
         }
     }
 
-    @Inject(method="actuallyHurt", at=@At("HEAD"))
+    @Inject(method = "actuallyHurt", at = @At("HEAD"))
     protected void cancelInvulnerabilityTimer(DamageSource damageSource, float f, CallbackInfo info) {
         if (!this.isInvulnerableTo(damageSource) && damageSource.getEntity() instanceof Player) {
             this.invulnerableTime = 0;
         }
     }
 
-    @Inject(method = "hurt", at = @At("HEAD"))
-    public void damageThroughShield(DamageSource damageSource, float f, CallbackInfoReturnable<Boolean> cir)
-    {
+    @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
+    public void damageThroughShield(DamageSource damageSource, float f, CallbackInfoReturnable<Boolean> cir) {
         float h = 0.0F;
         Entity entity2;
-        if(f > 0.0F && this.isDamageSourceBlocked(damageSource))
-        {
+        if (f > 0.0F && this.isDamageSourceBlocked(damageSource)) {
+
             h = Math.min(this.getBlockingItem().getTagElement("BlockEntityTag") != null ? 10.0F : 5.0F, f);
             if (!damageSource.isProjectile() && !damageSource.isExplosion()) {
                 entity2 = damageSource.getDirectEntity();
                 if (entity2 instanceof LivingEntity) {
-                    this.blockUsingShield((LivingEntity)entity2);
+                    this.blockUsingShield((LivingEntity) entity2);
+
                 }
             } else {
                 h = f;
             }
             f-=h;
-            
-            this.actuallyHurt(damageSource, f);
+            this.combatTracker.recordDamage(damageSource, this.getHealth(), f);
+            this.actuallyHurt(damageSource,f);
+            boolean bl2 = true;
+            if (this.isDeadOrDying()) {
+                if (!this.checkTotemDeathProtection(damageSource)) {
+                    SoundEvent soundEvent = this.getDeathSound();
+                    if (bl2 && soundEvent != null) {
+                        this.playSound(soundEvent, this.getSoundVolume(), this.getVoicePitch());
+                    }
 
+                    this.die(damageSource);
+                }
+            } else if (bl2) {
+                this.playHurtSound(damageSource);
+            }
+
+
+            cir.cancel();
         }
     }
 
     @Overwrite
-    public boolean isBlocking()
-    {
+    public boolean isBlocking() {
         return !this.getBlockingItem().isEmpty();
     }
 
@@ -104,7 +145,7 @@ public abstract class LivingEntityMixin extends Entity {
                 return this.useItem;
             }
         } //else if ((this.isOnGround() && this.isCrouching() || this.isPassenger())) {
-            //this should only be for if shield crouching is enabled, a setting for this should be added later, for now it will be on by default
+        //this should only be for if shield crouching is enabled, a setting for this should be added later, for now it will be on by default
 //            ItemStack itemStack = this.getItemInHand(InteractionHand.OFF_HAND);
 //            if (!itemStack.isEmpty() && itemStack.getItem().getUseAnimation(itemStack) == UseAnim.BLOCK) {
 //                return itemStack;
